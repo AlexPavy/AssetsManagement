2468ed5e0ded9741b9cfb0ef4c27c9ae193efec5
Alex Pavy <alex.g.pavy@gmail.com>
2018-04-27 03:07:56 +0100

    fixed resetAll

--- src/models/models.js
+++ src/models/models.js
@@ -5,15 +5,13 @@ const createModels = require('./model_definitions');
 const config = configurations.getConfig('database');
 const sequelize = new Sequelize(config.database, config.username, config.password, config);
 
-const { User, Allocation, Asset, AssetType } = createModels(sequelize, Sequelize);
+const {User, Allocation, Asset, AssetType} = createModels(sequelize, Sequelize);
 
 async function resetAll() {
-  return Promise.all([
-    User.destroy({ truncate : true, cascade: true }),
-    Asset.destroy({ truncate : true, cascade: true }),
-    AssetType.destroy({ truncate : true, cascade: true }),
-    Allocation.destroy({ truncate : true, cascade: true }),
-  ]);
+  await Allocation.destroy({truncate: true, cascade: true});
+  await User.destroy({truncate: true, cascade: true});
+  await Asset.destroy({truncate: true, cascade: true});
+  await AssetType.destroy({truncate: true, cascade: true});
 }
 
 const Models = {
